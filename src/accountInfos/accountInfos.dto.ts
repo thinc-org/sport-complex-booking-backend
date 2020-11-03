652bba9bd31d7e70309dff74dd93057eda95bc54
Pattanachai Chonglertvarawong <pattanachai_ac133@yahoo.com>
2020-11-03 20:39:05 +0700

    feature: Add data validation for accountInfos

--- src/accountInfos/accountInfos.dto.ts
+++ src/accountInfos/accountInfos.dto.ts
@@ -1,6 +1,6 @@
-import { IsBoolean, isDefined, IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsDate, isDefined, IsEmail, IsNotEmpty, IsOptional, isString, IsString, ValidateNested } from 'class-validator';
 import { Contact_person } from './accountInfo.schema';
-import {Schema as MongooseSchema} from 'mongoose'
+import { Schema as MongooseSchema } from 'mongoose'
 
 export class editAccountInfoDTO {
     @IsString()
@@ -11,19 +11,54 @@ export class editAccountInfoDTO {
     is_thai_language: boolean
 }
 
+export class ContactPersonDTO {
+    @IsString()
+    contact_person_prefix: string
+    @IsString()
+    contact_person_name: string
+    @IsString()
+    contact_person_surname: string
+    @IsString()
+    @IsOptional()
+    contact_person_home_phone: string
+    @IsString()
+    @IsOptional()
+    contact_person_phone: string
+}
+
 export class editOtherAccountInfoDTO {
+    @IsBoolean()
     is_thai_language: boolean
+    @IsString()
     prefix: string
+    @IsString()
     name: string
+    @IsString()
     surname: string
+    @IsDate()
     birthday: Date
+    @IsString()
     national_id: string
+    @IsString()
     gender: string
+    @IsString()
+    @IsOptional()
     marital_status: string
+    @IsString()
+    @IsOptional()
     address: string
+    @IsString()
+    @IsOptional()
     phone: string
+    @IsString()
+    @IsOptional()
     home_phone: string
+    @IsString()
+    @IsOptional()
     personal_email: string
-    contact_person: Contact_person
+    @ValidateNested()
+    contact_person: ContactPersonDTO;
+    @IsString()
+    @IsOptional()
     medical_condition: string
 }
