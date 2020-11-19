80defe064d586ffc4a580253ac3342a434b4e270
Chawak <56959186+Chawak@users.noreply.github.com>
2020-11-20 00:53:08 +0700

    Update approval.controller.ts
    now checking ifStaff in request

--- src/approval/approval.controller.ts
+++ src/approval/approval.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get ,Param,Patch,Body, UseGuards} from '@nestjs/common';
+import { Controller, Get ,Param,Patch,Body, UseGuards,Req,HttpStatus,HttpException} from '@nestjs/common';
 import { ApprovalService } from './approval.service';
 import {JwtAuthGuard } from 'src/auth/jwt.guard'
 @Controller('approval')
@@ -7,32 +7,37 @@ export class ApprovalController {
 
   @UseGuards(JwtAuthGuard)  
   @Get('/:start/:end')
-  getByRange(@Param('start') start:number,@Param('end') end:number){
+  getByRange(@Param('start') start:number,@Param('end') end:number,@Req() req){
+    if (!req.user.isStaff) throw new HttpException('Staff only', HttpStatus.UNAUTHORIZED)
     return this.approvalService.getByRange(start,end);
   }
   
   @UseGuards(JwtAuthGuard)  
   @Get("/:query/:start/:end")
   getSearchResult(@Param('query') query:string,@Param('start') start:number,
-                  @Param('end') end:number){
+                  @Param('end') end:number,@Req() req){
+    if (!req.user.isStaff) throw new HttpException('Staff only', HttpStatus.UNAUTHORIZED)
     return this.approvalService.getSearchResult(query,start,end);
   }
   
   @UseGuards(JwtAuthGuard)  
   @Get("/:username")
-  getPersonalData(@Param('username') username:string){
+  getPersonalData(@Param('username') username:string,@Req() req){
+    if (!req.user.isStaff) throw new HttpException('Staff only', HttpStatus.UNAUTHORIZED)
     return this.approvalService.getPersonalData(username);
   }
 
   @UseGuards(JwtAuthGuard)  
   @Patch("/approve")
-  approve(@Body('username') username:string,@Body('newExpiredDate') newExpiredDate:Date){
+  approve(@Body('username') username:string,@Body('newExpiredDate') newExpiredDate:Date,@Req() req){
+    if (!req.user.isStaff) throw new HttpException('Staff only', HttpStatus.UNAUTHORIZED)
     return this.approvalService.approve(username,newExpiredDate);
   }
 
   @UseGuards(JwtAuthGuard)  
   @Patch("/reject")
-  reject(@Body('username') username:string,@Body('reject_info') reject_info:[string]){
+  reject(@Body('username') username:string,@Body('reject_info') reject_info:[string],@Req() req){
+    if (!req.user.isStaff) throw new HttpException('Staff only', HttpStatus.UNAUTHORIZED)
     return this.approvalService.reject(username,reject_info);
   }
 }
