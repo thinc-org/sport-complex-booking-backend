b2e2cb47d7f8344ecef376f297094d3218c1a528
Watwe <anon.ongsakul2000@gmail.com>
2020-12-19 16:46:59 +0700

    ADD(Reservation) Reservation schema and interface

---
+++ src/successfulreservation/schema/successfulreservation.schema.ts
@@ -0,0 +1,15 @@
+import * as mongoose from "mongoose";
+
+const Booking_time = new mongoose.Schema({
+    start_time : Number,
+    end_time : Number
+});
+
+export const SuccesfulReservationSchema = new mongoose.Schema({
+    sport_id : mongoose.Types.ObjectId,
+    court_number : Number,
+    date : Date,
+    time_slot : [Booking_time],
+    list_member : [mongoose.Types.ObjectId],
+    is_check : Boolean
+});
