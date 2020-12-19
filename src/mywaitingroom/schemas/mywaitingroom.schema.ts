df60b0caf6cbce6fd45cef402975821209181ab0
Watwe <anon.ongsakul2000@gmail.com>
2020-12-19 16:32:37 +0700

    FIX(MyWaitingRoom) fix schema to simpler version.

--- src/mywaitingroom/schemas/mywaitingroom.schema.ts
+++ src/mywaitingroom/schemas/mywaitingroom.schema.ts
@@ -1,14 +1,16 @@
 import * as mongoose from "mongoose";
 
+const Booking_time = new mongoose.Schema({
+    start_time : Number,
+    end_time : Number
+});
+
 
 export const MyWaitingRoomSchema = new mongoose.Schema({
     sport_id : mongoose.Types.ObjectId,
     court_number : Number,
     date : Date,
-    time_slot : [{
-        start_time : Number,
-        end_time : Number
-    }],
+    time_slot : [Booking_time],
     list_member : [mongoose.Types.ObjectId],
     access_code : String,
     expired_date : Date
