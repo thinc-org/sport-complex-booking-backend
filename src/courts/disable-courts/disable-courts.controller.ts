bb8756ece0a23be8aae5228723ab64d1054f630e
Pattanachai Chonglertvarawong <pattanachai_ac133@yahoo.com>
2020-12-22 09:18:23 +0700

    refactor(COURT): Bind ValidationPipe at controller level

--- src/courts/disable-courts/disable-courts.controller.ts
+++ src/courts/disable-courts/disable-courts.controller.ts
@@ -4,11 +4,11 @@ import { AddDisableTimeDTO, CreateDisableCourtDTO, EditDisableCourtDTO, QueryDis
 import { DisableCourtsService } from './disable-courts.service';
 import { DisableCourt } from './interfaces/disable-courts.interface';
 
+@UsePipes(new ValidationPipe({transform: true}))
 @Controller('courts/disable-courts')
 export class DisableCourtsController {
     constructor(private readonly disableCourtsService: DisableCourtsService) { }
     
-    @UsePipes(new ValidationPipe({transform: true}))
     @UseGuards(JwtAuthGuard)
     @Post('')
     async createDisableCourt(@Req() req, @Body() body: CreateDisableCourtDTO): Promise<DisableCourt> {
@@ -16,25 +16,21 @@ export class DisableCourtsController {
         return await this.disableCourtsService.createDisableCourt(body);
     }
 
-    @UsePipes(new ValidationPipe({transform: true}))
     @Post('/search')
     async getDisableCourt(@Body() data: QueryDisableCourtDTO ): Promise<QueryResult> {
         return await this.disableCourtsService.queryDisableCourt(data);
     }
 
-    @UsePipes(new ValidationPipe({transform: true}))
     @Get('closed_time')
     async getClosedTime(@Req() req, @Query('sport_id') sport_id: string, @Query('court_num') court_num: number, @Query('date') dateString: string): Promise<Array<[number, number]>> {
         return await this.disableCourtsService.findClosedTimes(sport_id, court_num, new Date(dateString));
     }
 
-    @UsePipes(new ValidationPipe({transform: true}))
     @Get(':id')
     async getDisableCourtById(@Req() req, @Param('id') id: string): Promise<DisableCourt> {
         return await this.disableCourtsService.getDisableCourt(id);
     }
 
-    @UsePipes(new ValidationPipe({transform: true}))
     @UseGuards(JwtAuthGuard)
     @Delete('')
     async deleteAllDisableCourt(@Req() req): Promise<void> {
@@ -42,7 +38,6 @@ export class DisableCourtsController {
         await this.disableCourtsService.deleteAllDisableCourt();
     }
 
-    @UsePipes(new ValidationPipe({transform: true}))
     @UseGuards(JwtAuthGuard)
     @Delete(':id')
     async deleteDisableCourt(@Req() req, @Param('id') id: string): Promise<void> {
@@ -50,7 +45,6 @@ export class DisableCourtsController {
         await this.disableCourtsService.deleteDisableCourt(id);
     }
 
-    @UsePipes(new ValidationPipe({transform: true}))
     @UseGuards(JwtAuthGuard)
     @Put(':id')
     async editDisableCourt(@Req() req, @Param('id') id: string, @Body() body: EditDisableCourtDTO): Promise<DisableCourt> {
@@ -58,7 +52,6 @@ export class DisableCourtsController {
         return await this.disableCourtsService.editDisableCourt(id,body);
     }
 
-    @UsePipes(new ValidationPipe({transform: true}))
     @UseGuards(JwtAuthGuard)
     @Put(':id/add_disable_time')
     async addDisableTime(@Req() req, @Param('id') id: string, @Body() body: AddDisableTimeDTO){
