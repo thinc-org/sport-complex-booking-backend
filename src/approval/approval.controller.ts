9658d9baf6af7b00bf429dd87b6fbe23f5405b1c
Pattanachai Chonglertvarawong <pattanachai_ac133@yahoo.com>
2020-12-22 14:12:39 +0700

    refactor(APPROVAL): Remove unnecessary guards

--- src/approval/approval.controller.ts
+++ src/approval/approval.controller.ts
@@ -7,25 +7,21 @@ import {JwtAuthGuard, StaffGuard } from 'src/auth/jwt.guard'
 export class ApprovalController {
     constructor(private readonly approvalService: ApprovalService) {}
 
-  @UseGuards(JwtAuthGuard)  
   @Get()
   getSearchResult(@Query() query,@Req() req){
     return this.approvalService.getSearchResult(query.name,query.start,query.end);
   }
   
-  @UseGuards(JwtAuthGuard)  
   @Get("/:id")
   getPersonalData(@Param('id') id:string,@Req() req){
     return this.approvalService.getPersonalData(id);
   }
 
-  @UseGuards(JwtAuthGuard)  
   @Patch("/approve")
   approve(@Body('id') id:string,@Body('newExpiredDate') newExpiredDate:Date,@Req() req){
     return this.approvalService.approve(id,newExpiredDate);
   }
 
-  @UseGuards(JwtAuthGuard)  
   @Patch("/reject")
   reject(@Body('id') id:string,@Body('reject_info') reject_info:string[],@Req() req){
     return this.approvalService.reject(id,reject_info);
