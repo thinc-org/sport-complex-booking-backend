eb2c12023765ffd037972b10f778965faea7dab9
Natthorn <tum210544@gmail.com>
2020-12-23 22:55:15 +0700

    Fix reservation schema

--- src/reservation/dto/waiting-room.dto.ts
+++ src/reservation/dto/waiting-room.dto.ts
@@ -1,13 +1,8 @@
 import * as mongoose from 'mongoose';
 
-export interface Booking_time {
-    start_time : Number,
-    end_time : Number
-}
-
 export class WaitingRoomDto extends mongoose.Document {
     sport_id : mongoose.Types.ObjectId
     court_number : Number
     date : Date
-    time_slot : Booking_time[]
+    time_slot : Number[]
 }
