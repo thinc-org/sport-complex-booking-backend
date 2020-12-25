013e831cdb141681b8be171181791e4025696865
Natthorn <tum210544@gmail.com>
2020-12-25 23:49:55 +0700

    Using disable court service in check time slot

--- src/reservation/interfaces/reservation.interface.ts
+++ src/reservation/interfaces/reservation.interface.ts
@@ -3,20 +3,20 @@ import * as mongoose from "mongoose";
 
 export interface Reservation extends mongoose.Document {
     sport_id : mongoose.Types.ObjectId
-    court_number : Number
+    court_number : number
     date : Date
-    day_of_week : Number
-    time_slot : Number[]
+    day_of_week : number
+    time_slot : number[]
     list_member : mongoose.Types.ObjectId[]
-    is_check : Boolean
+    is_check : boolean
 }
 export interface WaitingRoom extends mongoose.Document {
     sport_id : mongoose.Types.ObjectId
-    court_number : Number
+    court_number : number
     date : Date
-    day_of_week : Number
-    time_slot : Number[]
+    day_of_week : number
+    time_slot : number[]
     list_member : mongoose.Types.ObjectId[]
-    access_code : String
+    access_code :string
     expired_date : Date
 }
