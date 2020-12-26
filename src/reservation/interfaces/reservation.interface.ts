208a8d6eb33ba9da7acd9790719de751e0b85643
sdsd <mondcha1507@gmail.com>
2020-12-26 14:01:08 +0700

    fix(RESERVATION) : add day field in schema

--- src/reservation/interfaces/reservation.interface.ts
+++ src/reservation/interfaces/reservation.interface.ts
@@ -14,8 +14,15 @@ export interface WaitingRoom extends mongoose.Document {
     sport_id : mongoose.Types.ObjectId
     court_number : number
     date : Date
+<<<<<<< HEAD
     day_of_week : number
     time_slot : number[]
+||||||| parent of 0942526... fix(RESERVATION) : add day field in schema
+    time_slot : Booking_time[]
+=======
+    day : Number
+    time_slot : Number[]
+>>>>>>> 0942526... fix(RESERVATION) : add day field in schema
     list_member : mongoose.Types.ObjectId[]
     access_code :string
     expired_date : Date
