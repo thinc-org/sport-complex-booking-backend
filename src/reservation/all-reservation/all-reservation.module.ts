6169df53721922b022279d5a2988d7c6ff07303d
sdsd <mondcha1507@gmail.com>
2020-12-29 17:11:55 +0700

    fix(APPROVAL,ALLRESERVATION) : import staffModule to use StaffGuard

--- src/reservation/all-reservation/all-reservation.module.ts
+++ src/reservation/all-reservation/all-reservation.module.ts
@@ -3,8 +3,9 @@ import { AllReservationService } from './all-reservation.service';
 import { AllReservationController } from './all-reservation.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import {ReservationSchema} from 'src/reservation/schema/reservation.schema'
+import { StaffsModule } from 'src/staffs/staffs.module';
 @Module({
-  imports:[MongooseModule.forFeature([{ name: 'Reservation', schema: ReservationSchema, collection: 'list_reservation' }])],
+  imports:[MongooseModule.forFeature([{ name: 'Reservation', schema: ReservationSchema, collection: 'list_reservation' }]),StaffsModule],
   providers: [AllReservationService],
   controllers: [AllReservationController]
 })
--- src/reservation/all-waiting-room/all-waiting-room.module.ts
+++ src/reservation/all-waiting-room/all-waiting-room.module.ts
@@ -3,8 +3,9 @@ import { AllWaitingRoomService } from './all-waiting-room.service';
 import { AllWaitingRoomController } from './all-waiting-room.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import {WaitingRoomSchema} from 'src/reservation/schema/reservation.schema'
+import { StaffsModule } from 'src/staffs/staffs.module';
 @Module({
-  imports:[MongooseModule.forFeature([{ name: 'WaitingRoom', schema: WaitingRoomSchema, collection: 'list_waiting_room' }])],
+  imports:[MongooseModule.forFeature([{ name: 'WaitingRoom', schema: WaitingRoomSchema, collection: 'list_waiting_room' }]),StaffsModule],
   providers: [AllWaitingRoomService],
   controllers: [AllWaitingRoomController]
 })
