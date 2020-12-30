03ba44597044a1e3fab82a7f7304a8af02a97b9c
Watwe <anon.ongsakul2000@gmail.com>
2020-12-30 09:53:09 +0700

    Fix(WaitingRoom) Add population process in waiting room fetching.

--- src/reservation/schema/reservation.schema.ts
+++ src/reservation/schema/reservation.schema.ts
@@ -11,12 +11,12 @@ export const ReservationSchema = new mongoose.Schema({
 });
 
 export const WaitingRoomSchema = new mongoose.Schema({
-    sport_id : mongoose.Types.ObjectId,
+    sport_id : {type : mongoose.Types.ObjectId , ref : 'Sport'},
     court_number : Number,
     date : Date,
     day_of_week : Number,
     time_slot : [Number],
-    list_member : [mongoose.Types.ObjectId],
+    list_member : [{type : mongoose.Types.ObjectId , ref : 'User'}],
     access_code : String,
     expired_date : Date
 });
