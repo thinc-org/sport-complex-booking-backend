029061928e66442c04d2fbf29ae548e8d10da28e
Pattanachai Chonglertvarawong <pattanachai_ac133@yahoo.com>
2021-01-07 01:01:10 +0700

    Fix(ACCOUNT_INFO): Check if personal_email is already used

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -17,6 +17,12 @@ export class UsersService {
     ) { }
 
     async validateAndEditAccountInfo(userId: string, updt, full: boolean) {
+        if(updt.personal_email != null) {
+            const found = await this.userModel.find({personal_email: updt.personal_email});
+            for (const user of found) {
+                if(user._id != userId) throw new HttpException('Email is already used', HttpStatus.CONFLICT);
+            }
+        }
         const user = await this.findById(userId);
         await user.validateAndEditAccountInfo(updt, full);
         return await user.save();
