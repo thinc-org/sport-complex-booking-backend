ae9feae239315fff518b192162275c4ff1dd2566
tum210544 <69684859+tum210544@users.noreply.github.com>
2021-02-25 01:37:48 +0700

    Revert "set up swagger for user/validation, staff&court manager"
    This reverts commit 08010ff6a738b3aa9521cc71b5e1b83f13c043fb.

--- src/court-manager/court-manager.controller.ts
+++ src/court-manager/court-manager.controller.ts
@@ -1,6 +1,3 @@
-import { ApiBadRequestResponse } from "@nestjs/swagger"
-import { ApiOkResponse } from "@nestjs/swagger"
-import { ApiTags } from "@nestjs/swagger"
 import { AdminGuard } from "./../auth/jwt.guard"
 import { StaffManagerService } from "./../staffs/staff-manager/staff-manager.service"
 import { JwtAuthGuard, StaffGuard } from "src/auth/jwt.guard"
@@ -10,14 +7,13 @@ import { Sport, Court } from "./interfaces/sportCourt.interface"
 import { Setting } from "./interfaces/setting.interface"
 import { ListAllUserService } from "./../staffs/list-all-user/list-all-user.service"
 import { Role } from "src/common/roles"
-import { CourtDTO, searchQueryDTO, searchResultDTO, SettingDTO, updateSportDTO, SportDTO } from "./dto/courts.dto"
 
 interface searchQuery {
   start: number
   end: number
   filter: string
 }
-@ApiTags("court-manager")
+
 @Controller("court-manager")
 export class CourtManagerController {
   constructor(
@@ -34,15 +30,12 @@ export class CourtManagerController {
   }
 
   @UseGuards(AdminGuard)
-  @ApiOkResponse({ description: "Setting updated", type: SettingDTO })
-  @ApiBadRequestResponse({ description: "Incorrect body", type: SettingDTO })
   @Put("setting")
   async updateSetting(@Body() new_setting: Setting): Promise<Setting> {
     return await this.courtManagerService.updateSetting(new_setting)
   }
 
   @UseGuards(JwtAuthGuard)
-  @ApiOkResponse({ description: "Setting received", type: SettingDTO })
   @Get("setting")
   async getSetting(@Req() req): Promise<Setting> {
     if (!(req.user.role == Role.Admin || req.user.role == Role.Staff) === false) {
@@ -57,16 +50,13 @@ export class CourtManagerController {
   //if no filter, use $ as param for filter
 
   @UseGuards(AdminGuard)
-  @ApiOkResponse({ description: "Query successful (an array of Court)", type: searchResultDTO })
-  @ApiBadRequestResponse({ description: "Incorrect search query", type: searchQueryDTO })
   @Get("/search") //    /search?start=<START>&end=<END>&filter=<FILTER>
   async getSportList(@Query() query: searchQuery): Promise<{ allSport_length: number; sport_list: Sport[] }> {
+    console.log(query)
     return await this.courtManagerService.sportRegexQuery(query.start, query.end, query.filter)
   }
 
   @UseGuards(JwtAuthGuard)
-  @ApiOkResponse({ description: "Query successful (an array of Court)", type: searchResultDTO })
-  @ApiBadRequestResponse({ description: "Incorrect search query", type: searchQueryDTO })
   @Get("/sports")
   async getAllSportList(@Req() req): Promise<Sport[]> {
     //check if user or staff exists + error handling
@@ -80,8 +70,6 @@ export class CourtManagerController {
 
   //can be use for courts displaying
   @UseGuards(AdminGuard)
-  @ApiOkResponse({ description: "Query successful", type: CourtDTO })
-  @ApiBadRequestResponse({ description: "Incorrect id." })
   @Get("/:id")
   async getSport(@Param("id") id: string): Promise<Sport> {
     return await this.courtManagerService.findSportByID(id)
@@ -89,8 +77,6 @@ export class CourtManagerController {
 
   //create new document for each sport using body as sport
   @UseGuards(AdminGuard)
-  @ApiOkResponse({ description: "Sport created", type: CourtDTO })
-  @ApiBadRequestResponse({ description: "Incorrect body", type: CourtDTO })
   @Post("/")
   async createSport(@Body() sport_data: Sport): Promise<Sport> {
     return await this.courtManagerService.createSport(sport_data)
@@ -98,8 +84,6 @@ export class CourtManagerController {
 
   //for updating sport_name_th, sport_name_en, quotas, required_users
   @UseGuards(AdminGuard)
-  @ApiOkResponse({ description: "Sport updated", type: CourtDTO })
-  @ApiBadRequestResponse({ description: "Incorrect body", type: CourtDTO })
   @Put("/:id")
   async updateSport(
     @Param("id") id: string,
@@ -110,8 +94,6 @@ export class CourtManagerController {
 
   //delete sport by sport's _id
   @UseGuards(AdminGuard)
-  @ApiOkResponse({ description: "Sport deleted", type: CourtDTO })
-  @ApiBadRequestResponse({ description: "Incorrect body", type: CourtDTO })
   @Delete(":id")
   async deleteSport(@Param("id") id: string): Promise<Sport> {
     return await this.courtManagerService.deleteSport(id)
@@ -119,8 +101,6 @@ export class CourtManagerController {
 
   //Param is sportType ex. Basketball (in english)
   @UseGuards(AdminGuard)
-  @ApiOkResponse({ description: "Sport created", type: SportDTO })
-  @ApiBadRequestResponse({ description: "Incorrect body", type: updateSportDTO })
   @Put("court-setting/update")
   async changeCourtSetting(@Body() new_court: { sport_id: string; new_setting: Court[] }): Promise<Sport> {
     return await this.courtManagerService.updateCourtbyID(new_court.sport_id, new_court.new_setting)
--- src/staffs/dto/create-staff.dto.ts
+++ src/staffs/dto/create-staff.dto.ts
@@ -1,44 +1,8 @@
-import { IsString } from "class-validator"
-import { IsNumber } from "class-validator"
-import { ApiProperty } from "@nestjs/swagger"
 import * as mongoose from "mongoose"
 export class CreateStaffDto extends mongoose.Document {
-  @ApiProperty()
   readonly name: string
-  @ApiProperty()
   readonly surname: string
-  @ApiProperty()
   readonly username: string
-  @ApiProperty()
   password: string
-  @ApiProperty()
   readonly is_admin: boolean
-}
-
-export class StaffBodyDTO {
-  @ApiProperty()
-  new_staff: CreateStaffDto
-}
-
-export class SearchQueryDTO {
-  @ApiProperty()
-  @IsNumber()
-  start: number
-  @ApiProperty()
-  @IsNumber()
-  end: number
-  @ApiProperty()
-  @IsString()
-  filter: string
-  @ApiProperty()
-  @IsNumber()
-  type: string
-}
-
-export class SearchResultDTO {
-  @ApiProperty()
-  @IsNumber()
-  allStaff_length: number
-  @ApiProperty()
-  staff_list: CreateStaffDto[]
 }
