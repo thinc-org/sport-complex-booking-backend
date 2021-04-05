dcf68daf6077c221eabbfd9d369a889667840a24
Pattanachai Chonglertvarawong <pattanachai_ac133@yahoo.com>
2021-04-06 01:11:01 +0700

    Feat: Upload file for satit users

--- src/fs/fs.interface.ts
+++ src/fs/fs.interface.ts
@@ -1,9 +1,13 @@
-import { Multer } from "multer";
+import { Multer } from "multer"
 
 export interface UploadedFiles {
-    user_photo?: Array<Express.Multer.File>
-    medical_certificate?: Array<Express.Multer.File>
-    national_id_house_registration?: Array<Express.Multer.File>
-    relationship_verification_document?: Array<Express.Multer.File>
-    payment_slip?: Array<Express.Multer.File>
+  user_photo?: Array<Express.Multer.File>
+  medical_certificate?: Array<Express.Multer.File>
+  national_id_house_registration?: Array<Express.Multer.File>
+  relationship_verification_document?: Array<Express.Multer.File>
+  payment_slip?: Array<Express.Multer.File>
+}
+
+export interface UploadedFilesSatit {
+  student_card_photo: Array<Express.Multer.File>
 }
