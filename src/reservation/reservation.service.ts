7afa38d9b51acfc52d1b4a7416bfafbbd94b4323
Natthorn <tum210544@gmail.com>
2021-11-23 13:48:42 +0700

    Change Waiting Room Access Code to number only

--- src/reservation/reservation.service.ts
+++ src/reservation/reservation.service.ts
@@ -202,7 +202,7 @@ export class ReservationService {
 
   makeid(length): string {
     let result = ""
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    const characters = "0123456789"
     const charactersLength = characters.length
     for (let i = 0; i < length; i++) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength))
