5c279ff7bff98d3c65826c02a57d601baf0a4143
Natthorn <tum210544@gmail.com>
2021-12-15 13:42:33 +0700

    Change staff authority in delete reservation and waiting room

--- src/reservation/all-waiting-room/all-waiting-room.controller.ts
+++ src/reservation/all-waiting-room/all-waiting-room.controller.ts
@@ -1,26 +1,29 @@
 import { Controller, Get, Param, Body, UseGuards, Delete, Post } from "@nestjs/common"
 import { AllWaitingRoomService } from "./all-waiting-room.service"
-import { StaffGuard } from "src/auth/jwt.guard"
+import { AdminGuard, StaffGuard } from "src/auth/jwt.guard"
 import { WaitingRoomDTO, SearchWaitingRoomResultDTO, SearchDTO } from "src/reservation/all-reservation/dto/all-reservation.dto"
 import {
-  ApiBearerAuth, ApiNotFoundResponse, ApiForbiddenResponse,
+  ApiBearerAuth,
+  ApiNotFoundResponse,
+  ApiForbiddenResponse,
   ApiOkResponse,
   ApiParam,
   ApiTags,
-  ApiUnauthorizedResponse, ApiBody
+  ApiUnauthorizedResponse,
+  ApiBody,
 } from "@nestjs/swagger"
 
 @ApiTags("all-waiting-room")
 @ApiBearerAuth()
 @ApiUnauthorizedResponse({ description: "User is not logged in" })
-@ApiForbiddenResponse({ description: "Must be staff to use this endpoints" })
-@UseGuards(StaffGuard)
 @Controller("all-waiting-room")
 export class AllWaitingRoomController {
-  constructor(private readonly allWaitingRoomService: AllWaitingRoomService) { }
+  constructor(private readonly allWaitingRoomService: AllWaitingRoomService) {}
 
   @ApiBody({ type: SearchDTO })
   @ApiOkResponse({ description: "Return query results", type: SearchWaitingRoomResultDTO })
+  @ApiForbiddenResponse({ description: "Must be staff to use this endpoints" })
+  @UseGuards(StaffGuard)
   @Post()
   getSearchResult(@Body() body) {
     return this.allWaitingRoomService.getWaitingRoomSearchResult(body)
@@ -29,6 +32,8 @@ export class AllWaitingRoomController {
   @ApiParam({ name: "id", type: String })
   @ApiOkResponse({ description: "Return a waiting room with id in param", type: WaitingRoomDTO })
   @ApiNotFoundResponse({ description: "Can't find a waiting room" })
+  @ApiForbiddenResponse({ description: "Must be staff to use this endpoints" })
+  @UseGuards(StaffGuard)
   @Get("/:id")
   getWaitingRoom(@Param("id") id: string) {
     return this.allWaitingRoomService.getWaitingRoom(id)
@@ -37,6 +42,8 @@ export class AllWaitingRoomController {
   @ApiParam({ name: "id", type: String })
   @ApiOkResponse({ description: "Delete and return a waiting room with id in param", type: WaitingRoomDTO })
   @ApiNotFoundResponse({ description: "Can't find a waiting room" })
+  @ApiForbiddenResponse({ description: "Must be admin to use this endpoints" })
+  @UseGuards(AdminGuard)
   @Delete("/:id")
   delete(@Param("id") id: string) {
     return this.allWaitingRoomService.deleteWaitingRoom(id)
